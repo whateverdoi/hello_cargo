0576bea8637eda963af8d9f6ba9515afd0e698fa
whateverdoi <3619737259@qq.com>
2026-01-29 23:44:03 +0800

    modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
     println!("Hello, world!");
+    let x = 5;
+    let y = 20;
+    println!("x 的值为 {}", x);
+    println!("y 的值为 {}", y);
+    let z = x + y;
+    println!("z 的值为 {}", z);
 }
